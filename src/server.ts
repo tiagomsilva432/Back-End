936fb72e26b5b426da5ea601e90bd85eed5d33f3
tiagomsilva432 <tiago.m.silva432@gmail.com>
2026-07-27 11:21:27 +0100

    fix: added fail safe for .env file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { app } from "./app.js";
 
-const BASE_URL = process.env.BASE_URL;
-const PORT = Number(process.env.PORT);
+const BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost' ;
+const PORT = Number(process.env.PORT) ? process.env.PORT : 3000;
 
 app.listen(PORT, () => {
     console.log(`🔥Server a correr em: ${BASE_URL}:${PORT}🔥`);
