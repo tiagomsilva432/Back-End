3a9eacd956c00e10de6c8aa2cbfdc353039133bc
Miguel Silva <msmas133@gmail.com>
2026-09-08 11:23:10 +0100

    feat: added basic CRUD methods in all repositories

--- src/repositories/employee-profile-repo.ts
+++ src/repositories/employee-profile-repo.ts
@@ -2,3 +2,24 @@ import { AppDataSource } from "../data-source.js";
 import { EmployeeProfile } from "../entities/EmployeeProfile.js";
 
 export const employeeProfileRepo = AppDataSource.getRepository(EmployeeProfile);
+
+export async function getEmployeeProfileByUserId(userId: string): Promise<EmployeeProfile | null> {
+    return await employeeProfileRepo.findOneBy({
+        userId
+    });
+}
+
+export async function getEmployeeProfilesByCompanyId(companyId: string): Promise<EmployeeProfile[]> {
+    return await employeeProfileRepo.find({
+        where: { user: { companyId } },
+        order: { firstName: "ASC", lastName: "ASC" }
+    });
+}
+
+export async function createEmployeeProfile(profile: EmployeeProfile) {
+    return await employeeProfileRepo.save(profile);
+}
+
+export async function updateEmployeeProfile(profile: EmployeeProfile) {
+    return await employeeProfileRepo.save(profile);
+}
